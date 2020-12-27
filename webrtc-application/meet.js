de07bf9b33c4b4d9076191305657c606cd7171a9
Sudeep Sehgal <sehgal.sudeep@gmail.com>
2020-12-28 01:59:53 +0530

    commit 2

--- webrtc-application/meet.js
+++ webrtc-application/meet.js
@@ -36,8 +36,7 @@ function initUI() {
         btnTxt = "Initiate";
 
         var getUrl = window.location;
-        var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-        //var baseUrl = 'file:///Users/sudeepsehgal/Desktop/webrtc-assignment/webrtc-application/web-meet.html';
+        var baseUrl = getUrl.protocol + "//" + getUrl.host + getUrl.pathname.split('?')[0];
         meetUrl = `${baseUrl}?cid=${callid}`;
     }
 
